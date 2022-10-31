7ce3f512a2622d02a3083aab47c10ba7a51b3a3a
Nahuel Olmos <nahuelolmos22@gmail.com>
2022-10-31 17:58:49 -0300

    commit para pasar a la

--- src/componentes/opiniones.jsx
+++ src/componentes/opiniones.jsx
@@ -18,9 +18,9 @@ export const Opiniones = () => {
       <br />
       <a href="https://www.instagram.com/s/aGlnaGxpZ2h0OjE3ODk4MzAxNTU2NjYwODk3?story_media_id=2915598665046897950_21934120250&igshid=MDJmNzVkMjY="  rel={"noreferrer"} target={"_blank"}>
         <button data-text="Awesome" class="button">
-          <span class="actual-text">&nbsp;opiniones2&nbsp;</span>
+          <span class="actual-text">&nbsp;opinione2&nbsp;</span>
           <span class="hover-text" aria-hidden="true">
-            &nbsp;opiniones2&nbsp;
+            &nbsp;opinione2&nbsp;
           </span>
         </button>
       </a>
