796c50bd6934954c58a3c28e5c044dc0263de775
Nahuel Olmos <nahuelolmos22@gmail.com>
2022-11-04 09:32:55 -0300

    agregar mas opciones

--- src/componentes/opiniones.jsx
+++ src/componentes/opiniones.jsx
@@ -1,40 +1,32 @@
 import React from "react";
 import "../estilos/opiniones.css";
+import datos from "../datos/Opiniones.js";
 export const Opiniones = () => {
   return (
-    <div>
+    <div className="contenedorOpiniones">
       <h1>Ve las opiniones de nuestros clientes </h1>
-
-      <br />
-      <a href="https://www.instagram.com/s/aGlnaGxpZ2h0OjE3ODk2OTQxNzE2MzUxNzMy?story_media_id=2718118314187736563_21934120250&igshid=MDJmNzVkMjY=" rel={"noreferrer"} target={"_blank"} >
-        <button data-text="Awesome" class="button">
-          <span class="actual-text">&nbsp;opiniones1&nbsp;</span>
-          <span class="hover-text" aria-hidden="true">
-            &nbsp;opiniones1&nbsp;
-          </span>
-        </button>
-      </a>
-      <p></p>
-      <br />
-      <a href="https://www.instagram.com/s/aGlnaGxpZ2h0OjE3ODk4MzAxNTU2NjYwODk3?story_media_id=2915598665046897950_21934120250&igshid=MDJmNzVkMjY="  rel={"noreferrer"} target={"_blank"}>
-        <button data-text="Awesome" class="button">
-          <span class="actual-text">&nbsp;opinione2&nbsp;</span>
-          <span class="hover-text" aria-hidden="true">
-            &nbsp;opinione2&nbsp;
-          </span>
-        </button>
-      </a>
-      <p></p>
-      <br />
-      <a href="https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTU4MDY0OTM2NDg1MDIy?story_media_id=2952931443563498833_21934120250&igshid=MDJmNzVkMjY="  rel={"noreferrer"} target={"_blank"}>
-        <button data-text="Awesome" class="button">
-          <span class="actual-text">&nbsp;Audios&nbsp;</span>
-          <span class="hover-text" aria-hidden="true">
-            &nbsp;Audios&nbsp;
-          </span>
-        </button>
-      </a>
+      {datos.map((datos) => (
+        <div key={datos.id}>
+          <br />
+          <a
+            href={datos.link}
+            rel={"noreferrer"}
+            target={"_blank"}
+          >
+            <button data-text="Awesome" class="button">
+              <span class="actual-text">&nbsp;{datos.nombre}&nbsp;</span>
+              <span class="hover-text" aria-hidden="true">
+                &nbsp;{datos.nombre}&nbsp;
+              </span>
+            </button>
+          </a>
+          <p></p>
+          <br />
+        </div>
+      ))}
+      {/*
+     
+ */}
     </div>
   );
 };
-
---
+++ src/datos/Opiniones.js
@@ -0,0 +1,46 @@
+var datosOpiniones = [
+  {
+    id: 1,
+    link: "https://www.instagram.com/s/aGlnaGxpZ2h0OjE3ODk2OTQxNzE2MzUxNzMy?story_media_id=2718118314187736563_21934120250&igshid=MDJmNzVkMjY=",
+    nombre: "opiniones1",
+  },
+  {
+    id: 2,
+    link: "https://www.instagram.com/s/aGlnaGxpZ2h0OjE3ODk4MzAxNTU2NjYwODk3?story_media_id=2915598665046897950_21934120250&igshid=MDJmNzVkMjY=",
+    nombre: "opiniones2",
+  },
+  {
+    id: 3,
+    link: "https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTU4MDY0OTM2NDg1MDIy?story_media_id=2952931443563498833_21934120250&igshid=MDJmNzVkMjY=",
+    nombre: "Audios",
+  },
+  {
+    id: 4,
+    link: "https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTIzNDk4OTU0ODA5MjE4?story_media_id=2681624461700482906_21934120250&igshid=MDJmNzVkMjY=",
+    nombre: "Casamiento",
+  },
+  {
+    id: 5,
+    link: "https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTE1ODAwNzI3MDE2OTYz?story_media_id=2688350283495710102_21934120250&igshid=MDJmNzVkMjY=",
+    nombre: "Infantiles",
+  },
+  {
+    id: 6,
+    link: "https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTI5MjY3MDc1Nzg1NTQy?story_media_id=2720864960563346204_21934120250&igshid=MDJmNzVkMjY=",
+    nombre: "18años",
+  },{
+    id: 7,
+    link: "https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTc3NTE0MDA1NDgwNDI1?story_media_id=2816625966210051923_21934120250&igshid=MDJmNzVkMjY=",
+    nombre: "Bumblebee",
+  },{
+    id: 8,
+    link: "https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTUxNjM5NTYzNDMyODMx?story_media_id=2544029912752517631_21934120250&igshid=MDJmNzVkMjY=",
+    nombre: "Clientes",
+  },{
+    id: 9,
+    link: "https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTUzMTE3MTc0NjAyNzU1?story_media_id=2859281087804689486_21934120250&igshid=MDJmNzVkMjY=",
+    nombre: "Cumpleaños",
+  },
+];
+
+export default datosOpiniones;
